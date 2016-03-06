dc972b8896202f2751ceb789ce44735f570e5c5b
hcyue <i@hcyue.me>
2016-03-06 11:14:08 +0800

    profile

--- tools/webpack.config.prod.js
+++ tools/webpack.config.prod.js
@@ -18,7 +18,7 @@ module.exports = {
   plugins: [
     new ExtractTextPlugin('bundle.css'),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"development"'
+      'process.env.NODE_ENV': '"production"'
     })
   ],
   module: {
--- view/containers/admin/Profile.jsx
+++ view/containers/admin/Profile.jsx
@@ -2,6 +2,15 @@ import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 
 export default class Profile extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            list: []
+        }
+    }
+    componentWillMount() {
+
+    }
     render() {
         return (
             <div className="Profile"></div>
