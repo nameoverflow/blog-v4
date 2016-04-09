367129e5e44a5c6b720e743dc021c3040cfa4b39
hcyue <i@hcyue.me>
2016-04-09 17:55:42 +0800

    ms

--- view/client.jsx
+++ view/client.jsx
@@ -23,7 +23,7 @@ const makeRequest = (url, opt) => {
     const { origin } = window.location
     const real_url = origin + url
     return fetch(url, opt)
-        .then(res => 
+        .then(res =>
                 res.ok ? res.json()
                     : {
                         err: res.statusText,
@@ -37,6 +37,10 @@ const
     middleware = applyMiddleware(apiFactory(makeRequest)),
     store = createStore(reducer, initial_state, middleware),
     history = syncHistoryWithStore(browserHistory, store)
+
+if (/Edge/.test(navigator.userAgent)) {
+    alert("检测到你在使用 Edge \n请跟我念：“ Chrome 大法好，退软保平安”")
+}
 
 match({ history, routes }, (error, redirectLocation, renderProps) => {
     console.log('matched once')
